1def57411f54bb715d80ce43ca10a9f394299926
JacobDNielsen <jacobpeternielsen@gmail.com>
2024-12-02 16:47:12 +0100

    Rating page now redirects to login, if not authorized. Aktsp now looks at JWT token from cookie + added error handling
    Still in progress

--- src/Component/Rating.js
+++ src/Component/Rating.js
@@ -4,48 +4,26 @@ import { GetTitle, GetTitleById } from '../Service/TitleService';
 import { useEffect, useState } from 'react';
 import { data } from 'react-router';
 import { useNavigate } from 'react-router';
+
 export default function Rating({...props}){
 
-    const {userId, titleId, rating} = props;
+    const {titleId, primaryTitle, posterUrl, rating} = props;
     //Later point, could get the createdAt and UpdateAt from prop?..
 
-    const [title, setTitle] = useState([]);
     const[errorMessage, setErrorMessage] = useState(null);
     const navigate = useNavigate();
 
-    const titleFetcher = async () => {
-        try {
-            const titleData = await GetTitleById(titleId); //Maybe change backend? This results in lots of calls to DB, if lots of ratings. Each rating gives 1 extra call to DB for GetTitleById
-            setTitle(titleData);
-            setErrorMessage(null); //resets error message
-            
-        } catch (error) {
-            console.log("Error in titleFetcher"+ error.status);
-            
-            setErrorMessage(error.message);
-        }
-    };
-
-   
-
-    useEffect(() => {
-
-        titleFetcher();
-        
-    },[titleId]); //What should state lookout for? Is update of state necessary?
-
     const onClickHandler = () => {
-        navigate("/title/"+title?.id);
+        navigate("/title/"+titleId);
     }
 
     return(
     <Card style={{ width: '18rem' }}>
      
-            <Card.Img variant="top" onClick = {title?.posterUrl ? onClickHandler : undefined} src={title?.posterUrl || "https://upload.wikimedia.org/wikipedia/commons/3/37/YouTube_loading_symbol_2_%28stable%29.gif"} alt={title?.primaryTitle}/> {/*Should be nullable or not?*/}
-           
+            <Card.Img variant="top" onClick = {posterUrl ? onClickHandler : undefined} src={posterUrl || "https://upload.wikimedia.org/wikipedia/commons/3/37/YouTube_loading_symbol_2_%28stable%29.gif"} alt={primaryTitle}/> {/*Should be nullable or not?*/}
                 <Card.Body>
-                <Card.Title style = {{color: 'red'}}>{errorMessage}</Card.Title> {/* shows the error message if truthy. Currently shows "Network Error", if refreshing page while pictures are loading..*/}
-                <Card.Title  style = {title?.primaryTitle ? {cursor: 'pointer'}: {color: 'red'}} onClick ={title?.primaryTitle ? onClickHandler : undefined}>{title?.primaryTitle || "Loading..."}</Card.Title> {/* Like with the img, Title of movie is only clickable when loaded */}
+                <Card.Title style = {{color: 'red'}}>{errorMessage}</Card.Title>
+                <Card.Title  style = {primaryTitle ? {cursor: 'pointer'}: {color: 'red'}} onClick ={primaryTitle ? onClickHandler : undefined}>{primaryTitle || "Loading..."}</Card.Title> {/* Like with the img, Title of movie is only clickable when loaded */}
                 <Card.Text> {`Rating: ${rating}` || "Rating not loaded"} </Card.Text>
                {/*<Button variant="primary">Go somewhere</Button>*/}
             </Card.Body>
--- src/Pages/UserRating.js
+++ src/Pages/UserRating.js
@@ -2,43 +2,73 @@ import {GetAllRatings} from '../Service/RatingService';
 import { useEffect, useState } from 'react';
 import { GetTitle } from "../Service/TitleService";
 import Rating from '../Component/Rating';
-
+import { useUser } from '../Store/store';
+import { useNavigate } from 'react-router';
 
 
 export default function UserRating(){
     const [userRatings, setUserRatings] = useState([]);
     const[errorMessage, setErrorMessage] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
-    
+
+    let navigate = useNavigate();
+
+    const {token} = useUser();
     
     useEffect(() =>{
         const fetchRatings = async () => {
 
-            try{
+          
 
-            const ratings = await GetAllRatings();
+            const ratings = await GetAllRatings(token);
          
-            setUserRatings(ratings);
+            if(ratings.success){
+            setUserRatings(ratings.data);
             setIsLoading(false); //should not render "loading" in UI when rating fetch is successful
-           
-        }
-        catch(error){
-                console.log("error in UserRating");
-                setErrorMessage(error);
+            }
+            else {
+                
                 setIsLoading(false);
-        }
-        }
 
+                switch (ratings.message) {
+                    case "401":
+                        setErrorMessage("401");
+                        //navigate("/login");
+                        break;
+                
+                    default:
+                        break;
+                }
+
+                
+            }
+            
+        }
          
         fetchRatings();
     }, []);
 
- 
+    useEffect(() => {
+        const errorCodeHandler = () => {
+            if (errorMessage === "401") {
+                console.log("Unauthorized. We redirect in 5 sec my frined");
+                setTimeout(() => {navigate("/login")}, 5000);  //Needs to be in lambda function, otherwise navigate fires imediately
+                
+            }
+        };
+
+        errorCodeHandler();
+    }, [errorMessage]);
+
     return (
      
         <>
      {
-    userRatings.map((u, i) => <Rating key={i} {...u} />) //need of key?, not currently used in Rating component 
+    errorMessage !== "401" && userRatings.map((u, i) => <Rating key={i} {...u} />) //need of key?, not currently used in Rating component 
+     
+     }
+      {
+    errorMessage === "401" && <p>You must sign in. Redirecting in 5 sec:</p> //need of key?, not currently used in Rating component 
      
      }
      </>
