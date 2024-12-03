a0203f757e0396e13a667320ae8570f3888d981f
JacobDNielsen <jacobpeternielsen@gmail.com>
2024-12-03 12:47:11 +0100

    Added sorting and Placeholder for Rating

--- src/Component/Rating.js
+++ src/Component/Rating.js
@@ -4,6 +4,9 @@ import { GetTitle, GetTitleById } from '../Service/TitleService';
 import { useEffect, useState } from 'react';
 import { data } from 'react-router';
 import { useNavigate } from 'react-router';
+import Placeholder from 'react-bootstrap/Placeholder';
+import TitlePlacerholder from './TitlePlaceholder';
+
 
 export default function Rating({...props}){
 
@@ -18,15 +21,21 @@ export default function Rating({...props}){
     }
 
     return(
+        <>
+    
+   
+   {/* STill in progress, should utilize TitlePlaceHolder */}
     <Card style={{ width: '18rem' }}>
-     
-            <Card.Img variant="top" onClick = {posterUrl ? onClickHandler : undefined} src={posterUrl || "https://upload.wikimedia.org/wikipedia/commons/3/37/YouTube_loading_symbol_2_%28stable%29.gif"} alt={primaryTitle}/> {/*Should be nullable or not?*/}
-                <Card.Body>
+        <Card.Img variant="top" onClick = {posterUrl ? onClickHandler : undefined} src={posterUrl || "https://upload.wikimedia.org/wikipedia/commons/3/37/YouTube_loading_symbol_2_%28stable%29.gif"} alt={primaryTitle}/> {/*Should be nullable or not?*/}
+            <Card.Body>
                 <Card.Title style = {{color: 'red'}}>{errorMessage}</Card.Title>
                 <Card.Title  style = {primaryTitle ? {cursor: 'pointer'}: {color: 'red'}} onClick ={primaryTitle ? onClickHandler : undefined}>{primaryTitle || "Loading..."}</Card.Title> {/* Like with the img, Title of movie is only clickable when loaded */}
                 <Card.Text> {`Rating: ${rating}` || "Rating not loaded"} </Card.Text>
                {/*<Button variant="primary">Go somewhere</Button>*/}
             </Card.Body>
-        </Card>
+        </Card>    
+
+     
+</>
         );
 }
---
+++ src/Component/TitlePlaceholder.js
@@ -0,0 +1,21 @@
+import { Card } from 'react-bootstrap';     
+import Placeholder from 'react-bootstrap/Placeholder';
+
+export default function TitlePlacerholder(){
+    return (
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/3/37/YouTube_loading_symbol_2_%28stable%29.gif" />
+            <Card.Body>
+            <Placeholder as={Card.Title} animation="glow">
+                <Placeholder xs={6} />
+            </Placeholder>
+            <Placeholder as={Card.Text} animation="glow">
+                <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
+                <Placeholder xs={6} /> <Placeholder xs={8} />
+            </Placeholder>
+            <Placeholder.Button variant="primary" xs={6} />
+            </Card.Body>
+        </Card>
+        );
+    
+};
