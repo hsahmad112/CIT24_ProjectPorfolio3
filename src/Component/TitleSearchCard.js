9f95d4c30ed7f7c32b0d9ed312735dff7e73f1cc
mskQI <156182962+mskQI@users.noreply.github.com>
2024-12-05 13:01:57 +0100

    titleSearchCard has data, need more work on

--- src/Component/TitleSearchCard.js
+++ src/Component/TitleSearchCard.js
@@ -1,17 +1,25 @@
+import { useEffect, useState } from 'react';
+import { Card } from 'react-bootstrap';
+
 export default function TitleSearchCard ({title}){
+    // const [backdropUrl, setBackdropUrl] = useState("./no-image.jpg");
+    // const imageUrl = process.env.REACT_APP_TMDB_API_IMAGE_LINK;  
+
+    // useEffect(()=>{
+    //     async function getBackdrop(){
+    //         //console.log("titleid: " + title.titleId)
+    //         const res = (await GetTitleBackdrop(title.titleId));
+    //         if(res?.profile_path) setBackdropUrl(res.profile_path);
+    //     } 
+    //     getBackdrop();
+    // }, [title]);
 
     return (
-        <div>
-            <p>
-                Title: {title.titleId}
-            </p>
-            <p>
-                Primary title: {title.primaryTitle}
-            </p>
-             <p>
-                Url: {title.url}
-            </p>
-        </div>
+
+        <Card style={{width: '100%', padding: '10px', height: '150px', margin: "10px"}}>
+            <Card.Img
+                src={title.url.posterUrl !== null ? title.url.posterUrl : "./no-image.jpg"} />{title.url}
+        </Card>
    
     );
 }
