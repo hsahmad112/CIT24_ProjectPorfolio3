61d2b97a5735e6bb977b802d029f5de0af4c243f
Hassan <hsahmad112@gmail.com>
2024-12-09 14:59:10 +0100

    adding names and primarytitles to PersonProfile and TitleProfile

--- src/Component/TitleProfile.js
+++ src/Component/TitleProfile.js
@@ -8,14 +8,14 @@ export default function TitleProfile(title){
 
     const navigate = useNavigate();
      
-
+console.log("PRIMT ",title.data);
 return(
     <div className="d-flex align-items-center no-border">
             <Row>
             <Col>
             <Card style={{ width: '16rem', margin: '10px', padding: '0px'}}  onClick={()=> navigate("/title/" + title.data.titleId)}>
                 <Card.Body>
-                    <Card.Title> {title.data.titleId} </Card.Title>
+                    <Card.Title> {title.data.titlePrimaryTitle} </Card.Title>
                     <Card.Text> {title.data.annotation} </Card.Text>
                     <Button variant="primary">Go to title</Button>
                 </Card.Body>
